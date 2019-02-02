373ae80c0f0de4420a4c01914b186d1312ebf365
cpscott1 <thacam18@gmail.com>
2019-02-02 11:28:24 -0600

    adding mouse events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+const funBusHeader = document.querySelector('.fun-bus-header');
+
+funBusHeader.addEventListener('mouseover', event => {
+  funBusHeader.style.color = '#F8E5D7';
+})
+
+funBusHeader.addEventListener('mouseout', event => {
+  funBusHeader.style.color = 'black';
+})
