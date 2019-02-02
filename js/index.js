bd0d9fd654bf7ecd573dbfe333b40869696836ac
cpscott1 <thacam18@gmail.com>
2019-02-02 12:55:29 -0600

    added more events

--- js/index.js
+++ js/index.js
@@ -7,3 +7,30 @@ funBusHeader.addEventListener('mouseover', event => {
 funBusHeader.addEventListener('mouseout', event => {
   funBusHeader.style.color = 'black';
 })
+
+const mainNav = document.querySelector('.nav');
+
+mainNav.addEventListener('dblclick', event => {
+  mainNav.style.display = 'none';
+})
+
+const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('mousemove', event => {
+logo.style.color = '#B2D6E5';
+})
+
+
+const busImg = document.querySelector('.fun-bus-img');
+
+busImg.addEventListener('click', event => {
+  busImg.src = 'img/fun.jpg';
+})
+
+const funBusButton = document.querySelectorAll('.btn');
+
+for(var i= 0; i < funBusButton.length; i++){
+  funBusButton[i].addEventListener('mousedown', event => {
+  event.target.style.color = '#F8E0CF';
+  })
+}
